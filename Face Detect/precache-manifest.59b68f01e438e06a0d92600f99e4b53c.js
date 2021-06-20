e5d8e48ca3f4f6d12f6bb1ea32bff94f7d4ecb05
forlediska <37329950+forlediska@users.noreply.github.com>
2021-06-20 17:19:21 +0700

    Add files via upload

---
+++ Face Detect/precache-manifest.59b68f01e438e06a0d92600f99e4b53c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13c761fd919658b19f0d9410c234b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d03d4b988e7a8d6aa39",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "cae61da56edfea7a1c4d",
+    "url": "/static/css/main.4e622f55.chunk.css"
+  },
+  {
+    "revision": "0d03d4b988e7a8d6aa39",
+    "url": "/static/js/2.ea4a5471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea4a5471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae61da56edfea7a1c4d",
+    "url": "/static/js/main.eb9bb03a.chunk.js"
+  },
+  {
+    "revision": "01fefba12108911d1878",
+    "url": "/static/js/runtime-main.28f5bb8f.js"
+  }
+]);
